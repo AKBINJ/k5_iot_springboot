46402c227acb5e972193c0b3f72bec9f079853be
AKBINJ <jyp10311@naver.com>
2025-08-26 17:25:05 +0900

    20250826

--- src/main/java/com/example/k5_iot_springboot/repository/G_UserRepository.java
+++ src/main/java/com/example/k5_iot_springboot/repository/G_UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.k5_iot_springboot.repository;
 
-public class G_UserRepository {
+import com.example.k5_iot_springboot.entity.G_User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface G_UserRepository extends JpaRepository<G_User, Long> {
+    Optional<G_User> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
